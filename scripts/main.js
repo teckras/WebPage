4d6f2b630053a8ed675def94bd7e5f1c94352d2c
teckras <samy.iralour@gmail.com>
2017-10-21 22:16:17 +0100

    Almost working version

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,59 @@
 document.onkeypress = function(e){
-    if (e.code === "Space"){
+    var hasStarted = document.querySelector("#HasStarted");
+    hasStarted.textContent = "true";
+    if (hasStarted.textContent == "true"){
         attentionTest(e);
      }
 
 }
 
 
-function attentionTest(e){
-    var startTime = new Date().getTime();
-    var currentTime = startTime;
+function Start(){
+    var hasStarted = document.querySelector("#HasStarted");
+    hasStarted.textContent = "true";
     var charDisplayed = document.querySelector("#TestArea");
-    //while ((currentTime - startTime) <= 60000)
+    charDisplayed.textContent = "A";
+    var startTime = new Date().getTime();
+    var startTimeField = document.querySelector("#StartTime");
+    startTimeField.textContent = startTime.toString();
+    var errorField =  document.querySelector("#NumberOfErrors");
+    errorField.textContent = "0";
+    
+}
+
+function attentionTest(e){
+    var currentTime = new Date().getTime();
+    var startTime = parseInt(document.querySelector("#StartTime").textContent);
+    var charDisplayed = document.querySelector("#TestArea");
+    var currentCharacter = charDisplayed.textContent;
+    var isSpaceStruck = e.code == "Space";
+    var isRightArrowStruck = e.code == "ArrowRight";
+    var errorField =  document.querySelector("#NumberOfErrors");
+    var errorNumber = parseInt(errorField.textContent);
+    var currentCharacterListField = document.querySelector("#ListOfCharacters");
+    var currentCharacterList = currentCharacterListField.textContent;
+    currentCharacterList += currentCharacter;
+    currentCharacterListField.textContent = currentCharacterList;
+
+    if(currentCharacter == "X" && isSpaceStruck){
+        errorNumber += 1;
+    }else if (currentCharacter != "X" && isRightArrowStruck){
+        errorNumber += 1;
+    }
+    errorField.textContent = errorNumber;
+    if ((currentTime - startTime) <= 60000)
     {
         var rand = Math.random();
         var charNumberFloat =  65 + 26 * rand;
         var charNumber = Math.floor(charNumberFloat);
         
         charDisplayed.textContent = String.fromCharCode(charNumber);
+    }else{
+        var hasStarted = document.querySelector("#HasStarted");
+        hasStarted.textContent = "false";
+        charDisplayed.textContent = "Test is now completed, thank you"
     }
-    //charDisplayed.textContent = "Test is now completed, thank you"
+
 }
 
 function wait(ms){
